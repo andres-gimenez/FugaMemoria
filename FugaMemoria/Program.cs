fec911353c30d877417d1d332820f554f0c424d8
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-11-09 12:45:58 +0100

    Agregar archivos de proyecto.

---
+++ FugaMemoria/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FugaMemoria
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Inicando carga!");
+
+            List<byte[]> datos = new List<byte[]>();
+            Random rnd = new Random();
+            Stopwatch timeMeasure = new Stopwatch();
+            timeMeasure.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Stopwatch timeMeasure2 = new Stopwatch();
+                timeMeasure2.Start();
+                byte[] buffer = new byte[10000000];
+                rnd.NextBytes(buffer);
+                timeMeasure2.Stop();
+                Console.WriteLine($"buffer: {i} - {timeMeasure2.Elapsed.TotalMilliseconds}");
+                datos.Add(buffer);
+            }
+            timeMeasure.Stop();
+            Console.WriteLine($"Tiempo: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+
+            return 0;
+        }
+    }
+}
